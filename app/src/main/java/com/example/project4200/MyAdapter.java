84af86db8331e541fc8dd28e1511aa02ea287104
Kilian Huellen <khuellen@mail.uni-mannheim.de>
2023-03-21 01:49:51 -0400

    Smaller layout fixes. Still no use of other classes that I did not directly work on.

--- app/src/main/java/com/example/project4200/MyAdapter.java
+++ app/src/main/java/com/example/project4200/MyAdapter.java
@@ -3,9 +3,11 @@ package com.example.project4200;
 import static androidx.core.content.ContextCompat.startActivity;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.content.Intent;
@@ -15,7 +17,9 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
@@ -39,7 +43,12 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
         Appointment appointment = appointmentList.get(position);
         holder.title.setText(appointment.getTitle());
-        holder.date_and_time.setText(appointment.getTime().toString());
+        holder.date.setText(appointment.getTime().toString());
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            holder.days.setText("" + appointment.getTime().getDayOfYear());
+        }
+
 
         holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,15 +69,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         TextView title;
-        TextView date_and_time;
+        TextView date;
+        TextView days;
         CardView cardView;
+        ImageView imageView;
 
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.textView_title);
-            date_and_time = itemView.findViewById(R.id.textView_date_time);
+            date = itemView.findViewById(R.id.textView_date);
             cardView = itemView.findViewById(R.id.cardView);
+            days = itemView.findViewById(R.id.textView_days);
+            imageView = itemView.findViewById(R.id.imageView);
         }
 
     }
