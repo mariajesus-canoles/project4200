e8d9f066219e02c6b639522325d7220a45ff5111
Kilian Huellen <khuellen@mail.uni-mannheim.de>
2023-03-21 02:08:07 -0400

    one bug fix in each of the two files

--- app/src/main/java/com/example/project4200/DataBase.java
+++ app/src/main/java/com/example/project4200/DataBase.java
@@ -4,7 +4,7 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Picture.class, Event.class}, version = 1)
-public abstract class Database extends RoomDatabase {
+public abstract class DataBase extends RoomDatabase {
     public abstract AllDAO allDAO();
 
 }
